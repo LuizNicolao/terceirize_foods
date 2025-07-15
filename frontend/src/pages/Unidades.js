4118b8934ee2a742b1c49dc8e018e4ce3230890b
LuizNicolao <nicolaolf@gmail.com>
2025-07-15 17:23:17 -0300

    ajuste api

--- frontend/src/pages/Unidades.js
+++ frontend/src/pages/Unidades.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
-import { api } from '../services/api';
+import api from '../services/api';
 
 const Container = styled.div`
   padding: 20px;
