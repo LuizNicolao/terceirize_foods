4c7c537c2384cd811a2dc2921bb366da70a7ab51
LuizNicolao <nicolaolf@gmail.com>
2025-08-08 08:18:47 -0300

    a

--- foods/frontend/src/App.js
+++ foods/frontend/src/App.js
@@ -48,7 +48,7 @@ const PublicRoute = ({ children }) => {
   }
 
   if (isAuthenticated) {
-    return <Navigate to="/foods" replace />;
+    return <Navigate to="/" replace />;
   }
 
   return children;
