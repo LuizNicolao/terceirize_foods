13b54fc71c220d84159635eb40304a5800622b64
LuizNicolao <nicolaolf@gmail.com>
2025-08-14 15:40:35 -0300

    Update useProdutoGenerico.js

--- foods/frontend/src/hooks/useProdutoGenerico.js
+++ foods/frontend/src/hooks/useProdutoGenerico.js
@@ -4,7 +4,7 @@
  */
 
 import { useState, useEffect, useCallback } from 'react';
-import { toast } from 'react-toastify';
+import toast from 'react-hot-toast';
 import produtoGenericoService from '../services/produtoGenerico';
 import api from '../services/api';
 
