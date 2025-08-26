38ced19158b616aee874a9222ddac406e90c275d
LuizNicolao <nicolaolf@gmail.com>
2025-08-26 16:43:29 -0300

    Update Supervisor.jsx

--- cotacao/frontend/src/pages/supervisor/Supervisor.jsx
+++ cotacao/frontend/src/pages/supervisor/Supervisor.jsx
@@ -10,7 +10,8 @@ import { useSupervisor } from '../../hooks/useSupervisor';
 import { useAuth } from '../../contexts/AuthContext';
 import { usePermissions } from '../../contexts/PermissionsContext';
 import { Button, CadastroFilterBar, LoadingSpinner } from '../../components/ui';
-import { SupervisorStats, SupervisorTable } from './components';
+import SupervisorStats from './components/SupervisorStats.jsx';
+import SupervisorTable from './components/SupervisorTable.jsx';
 
 const Supervisor = () => {
   const navigate = useNavigate();
