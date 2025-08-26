1f83a3c6ad997f932d2dbef69cb319f792671011
LuizNicolao <nicolaolf@gmail.com>
2025-08-26 16:58:16 -0300

    Update index.js

--- cotacao/frontend/src/pages/supervisor/components/index.js
+++ cotacao/frontend/src/pages/supervisor/components/index.js
@@ -1,9 +1,3 @@
-// Componentes de análise
-export * from './analisar';
-
-// Componentes de visualização
-export * from './visualizacoes';
-
 // Componentes da página principal do supervisor
-// export { default as SupervisorStats } from './SupervisorStats.jsx';
-// export { default as SupervisorTable } from './SupervisorTable.jsx';
+export { default as SupervisorStats } from './SupervisorStats.jsx';
+export { default as SupervisorTable } from './SupervisorTable.jsx';
