adc9a5af9fe3a85a86bab59ac61306b3b9a8c598
LuizNicolao <nicolaolf@gmail.com>
2025-08-28 15:42:43 -0300

    Update EfetivoModal.jsx

--- foods/frontend/src/components/efetivos/EfetivoModal.jsx
+++ foods/frontend/src/components/efetivos/EfetivoModal.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import { Modal, Button, Input } from '../ui';
 import IntoleranciasService from '../../services/intolerancias';
-import UnidadesEscolaresService from '../../services/unidades-escolares';
+import UnidadesEscolaresService from '../../services/unidadesEscolares';
 
 const EfetivoModal = ({ 
   isOpen, 
