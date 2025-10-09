3f37f912383bc123016845d8b5e4de266670afb9
LuizNicolao <nicolaolf@gmail.com>
2025-10-09 09:32:17 -0300

    fix: corrigir exports de receitas/index.js
    ✅ Exportando métodos individuais ao invés de controllers inteiros
    ✅ Padrão alinhado com outros controllers
    
    Backend deve iniciar agora! 🚀

--- foods/backend/controllers/receitas/index.js
+++ foods/backend/controllers/receitas/index.js
@@ -8,9 +8,19 @@ const ReceitasCRUDController = require('./ReceitasCRUDController');
 const ReceitasExportController = require('./ReceitasExportController');
 
 module.exports = {
-  ReceitasListController,
-  ReceitasCRUDController,
-  ReceitasExportController,
+  // Métodos de Listagem
+  listar: ReceitasListController.listar,
+  buscarPorId: ReceitasListController.buscarPorId,
+  listarReceitas: ReceitasListController.listarReceitas,
+  buscarReceitaPorId: ReceitasListController.buscarReceitaPorId,
+  
+  // Métodos CRUD
+  criar: ReceitasCRUDController.criar,
+  atualizar: ReceitasCRUDController.atualizar,
+  excluir: ReceitasCRUDController.excluir,
+  criarReceita: ReceitasCRUDController.criarReceita,
+  atualizarReceita: ReceitasCRUDController.atualizarReceita,
+  excluirReceita: ReceitasCRUDController.excluirReceita,
   
   // Métodos de Exportação
   exportarXLSX: ReceitasExportController.exportarXLSX,
