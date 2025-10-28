b8dea69c982ee9dbea5d8f539cace2d5bcbd8f4d
LuizNicolao <nicolaolf@gmail.com>
2025-10-28 11:20:30 -0300

    fix: adicionar exportações dos componentes SubstituicoesTable
    - Adicionar SubstituicoesTableNutricionista e SubstituicoesTableCoordenacao no index.js
    - Corrigir erro de import durante build do Docker

--- implantacao/frontend/src/pages/necessidades-substituicoes/components/index.js
+++ implantacao/frontend/src/pages/necessidades-substituicoes/components/index.js
@@ -1,2 +1,4 @@
 export { default as SubstituicoesFilters } from './SubstituicoesFilters';
 export { default as SubstituicoesTable } from './SubstituicoesTable';
+export { default as SubstituicoesTableNutricionista } from './SubstituicoesTableNutricionista';
+export { default as SubstituicoesTableCoordenacao } from './SubstituicoesTableCoordenacao';
