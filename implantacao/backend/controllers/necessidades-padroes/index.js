c760b248fee0c03855e2c152f7ff8e75400b349f
LuizNicolao <nicolaolf@gmail.com>
2025-10-29 16:12:58 -0300

    fix: adicionar index.js para controllers necessidades-padroes
    Problema:
    - Módulo '../../controllers/necessidades-padroes' não encontrado
    - Causava erro MODULE_NOT_FOUND no backend
    
    Solução:
    - Criado index.js no diretório controllers/necessidades-padroes/
    - Exporta todos os controllers necessários
    - Segue padrão dos outros diretórios de controllers
    
    Benefício:
    - Backend iniciando corretamente
    - Estrutura consistente com outros módulos
    - Imports funcionando adequadamente

---
+++ implantacao/backend/controllers/necessidades-padroes/index.js
@@ -0,0 +1,12 @@
+/**
+ * Necessidades Padrões - Controllers
+ * Centraliza todos os controllers relacionados a necessidades padrões
+ */
+
+const NecessidadesPadroesListController = require('./NecessidadesPadroesListController');
+const NecessidadesPadroesCRUDController = require('./NecessidadesPadroesCRUDController');
+
+module.exports = {
+  NecessidadesPadroesListController,
+  NecessidadesPadroesCRUDController
+};
