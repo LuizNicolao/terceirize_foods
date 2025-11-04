faebfad964e903f91a1b1b6f1bfec85ca49e3802
LuizNicolao <nicolaolf@gmail.com>
2025-11-04 08:44:16 -0300

    fix: adicionar index.js na pasta de rotas de formas-pagamento
    - Criar arquivo index.js para exportar o router
    - Seguir padrão das outras rotas do projeto
    - Corrigir erro de módulo não encontrado

---
+++ foods/backend/routes/formas-pagamento/index.js
@@ -0,0 +1,9 @@
+/**
+ * Rotas de Formas de Pagamento
+ * Centraliza todas as rotas relacionadas a formas de pagamento
+ */
+
+const formasPagamentoRoute = require('./formasPagamentoRoute');
+
+module.exports = formasPagamentoRoute;
+
