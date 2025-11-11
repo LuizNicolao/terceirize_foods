95952cd985a888a2a37e1d81ed719399eb43afa1
LuizNicolao <nicolaolf@gmail.com>
2025-11-11 12:44:36 -0300

    chore(cotacao): remover páginas antigas de usuários e permissões

--- cotacao/frontend/src/pages/permissoes/components/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Exportações dos componentes da página de Permissões
- */
-
-export { default as PermissoesHeader } from './PermissoesHeader';
-export { default as PermissoesFilters } from './PermissoesFilters';
--- cotacao/frontend/src/pages/usuarios/components/index.js
+++
@@ -1,6 +0,0 @@
-/**
- * Exportações dos componentes da página de Usuários
- */
-
-export { default as UsuariosHeader } from './UsuariosHeader';
-export { default as UsuariosFilters } from './UsuariosFilters';
